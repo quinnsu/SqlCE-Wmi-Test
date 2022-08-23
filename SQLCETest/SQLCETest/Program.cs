6088534159275fd99e1dcbc6a0b2a4bd7a50637c
quinnsu <2509253965@qq.com>
2022-08-23 15:31:38 +0800

    test code

---
+++ SQLCETest/SQLCETest/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Diagnostics;
+
+namespace SQLCETest
+{
+    class Program
+    {
+        const int InsertTestCount = 100;
+        static void Main(string[] args)
+        {
+            //create null connection
+            SqlCeConnection conn = null;
+
+            string databaseName = "TestSQLCE.sdf";
+            if (File.Exists(databaseName))
+                File.Delete(databaseName);
+
+            //create database file
+            SqlCeEngine eng = new SqlCeEngine($"Data Source='{databaseName}'");
+            eng.CreateDatabase();
+
+            try
+            {
+                //connect to the database
+                conn = new SqlCeConnection($"Data Source='{databaseName}'");
+                conn.Open();
+
+                //CREATE TABLE
+                SqlCeCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "CREATE TABLE TestTbl(Pluginid NVARCHAR(100) PRIMARY KEY, PluginName NVARCHAR(100), ExecuteTotalCount INT, ExecuteExceptionCount INT,ExecuteMinDuration BIGINT, ExecuteAvgDuration BIGINT, ExecuteMaxDuration BIGINT)";
+                int affectRow = cmd.ExecuteNonQuery();
+
+                Console.WriteLine($"affectRow: {affectRow}");
+
+                //INSERT
+                insertRecords(cmd,conn);
+                //UPDATE
+                updateRecords(cmd);
+                //SEARCH
+                SearchAndCastRecords(cmd);
+                //DELETE
+                deleteRecords(cmd);
+
+
+                conn.Close();
+            }
+            catch (SqlCeException ex)
+            {
+                Console.WriteLine($"exception when execute {ex.Message} {ex.ToString()}");
+                foreach (SqlCeError error in ex.Errors)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("Press any key to continue...");
+            }
+        }
+
+        public static void insertRecords(SqlCeCommand cmd, SqlCeConnection conn)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < InsertTestCount; i++)
+            {
+                string pluginId = Guid.NewGuid().ToString();
+                cmd = new SqlCeCommand($"INSERT INTO TestTbl  VALUES ('{pluginId}','test-name', 0 , 0 , 0 , 0 , 0)", conn);
+                int affectRow = cmd.ExecuteNonQuery();
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds,
+                                                ts.Milliseconds);
+            Console.WriteLine($"Inserting {InsertTestCount} metrics execute result takes {elaspedTime} ");
+
+        }
+        public static void deleteRecords(SqlCeCommand cmd)
+        {
+            //delete
+            Stopwatch deletewatch = new Stopwatch();
+            deletewatch.Start();
+            cmd.CommandText = "DELETE FROM TestTbl WHERE PluginName = 'test-name'";
+            int affectRow = cmd.ExecuteNonQuery();
+            deletewatch.Stop();
+            TimeSpan ts = deletewatch.Elapsed;
+            string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                    ts.Hours, ts.Minutes, ts.Seconds,
+                                                    ts.Milliseconds);
+            Console.WriteLine($"delete {affectRow} lines records from the table, takes {elaspedTime} ms");
+        }
+        public static void updateRecords(SqlCeCommand cmd)
+        {
+            //UPDATE
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            cmd.CommandText = "UPDATE TestTbl SET ExecuteTotalCount = 1, ExecuteExceptionCount = 1, ExecuteMinDuration = 1, ExecuteAvgDuration = 1, ExecuteMaxDuration = null WHERE PluginName = 'test-name'";
+            int affectRow = cmd.ExecuteNonQuery();
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                    ts.Hours, ts.Minutes, ts.Seconds,
+                                                    ts.Milliseconds);
+            Console.WriteLine($"update {affectRow} lines records from the table, takes {elaspedTime} ms");
+
+        }
+        public static void SearchAndCastRecords(SqlCeCommand cmd)
+        {
+            //READ DATA
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            cmd.CommandText = ("SELECT * FROM TestTbl");
+            SqlCeDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var metrics = new PluginExecuteMetrics
+                {
+                    PluginId = reader["PluginId"].ToString(),
+                    PluginName = reader["PluginName"].ToString(),
+                    ExecuteTotalCount = Convert.ToInt32(reader["ExecuteTotalCount"]),
+                    ExecuteExceptionCount = Convert.ToInt32(reader["ExecuteExceptionCount"]),
+                    ExecuteMinDuration = Convert.ToInt64(reader["ExecuteMinDuration"]),
+                    ExecuteAvgDuration = Convert.ToInt64(reader["ExecuteAvgDuration"]),
+                    ExecuteMaxDuration = Convert.ToInt64(reader["ExecuteMaxDuration"])
+                };
+            }
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                    ts.Hours, ts.Minutes, ts.Seconds,
+                                                    ts.Milliseconds);
+            Console.WriteLine($"search {InsertTestCount} lines records from the table, takes {elaspedTime} ms");
+        }
+        public static void AlterTable_add(SqlCeCommand cmd)
+        {
+            cmd.CommandText = ("ALTER TABLE TestTbl Add description NVARCHAR(100)");
+            int affectRow = cmd.ExecuteNonQuery();
+            Console.WriteLine($"{affectRow} rows have been affected.");
+        }
+        public static void AlterTable_drop(SqlCeCommand cmd)
+        {
+            cmd.CommandText = ("ALTER TABLE TestTbl DROP COLUMN description");
+            int affectRow = cmd.ExecuteNonQuery();
+            Console.WriteLine($"{affectRow} rows have been affected.");
+        }
+    }
+
+    public class PluginExecuteMetrics
+    {
+        public string PluginId { get; set; }
+        public string PluginName { get; set; }
+        public int ExecuteTotalCount { get; set; }
+        public int ExecuteExceptionCount { get; set; }
+        public long ExecuteMinDuration { get; set; }
+        public long ExecuteAvgDuration { get; set; }
+        public long ExecuteMaxDuration { get; set; }
+
+        public override string ToString() => this.PluginId + this.PluginName + this.ExecuteTotalCount;
+
+    }
+}
---
+++ WMITest/ConsoleApp1/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Management;
+using System.Diagnostics;
+
+namespace TestWmi
+{
+    class Program
+    {
+        public const string WmiClientAgentPath = "\\root\\cmd\\clientagent";
+        public const int InsertTestCount = 5000;
+        static void Main(string[] args)
+        {
+            testAddWmiRecords();
+            testUpdateWmiRecords();
+            testSearchAndCastRecord();
+            testdeleteAllWmiRecords();
+
+        }
+
+        public static void testdeleteAllWmiRecords()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                       WmiClientAgentPath,
+                       "SELECT * FROM PluginExecuteMetrics WHERE PluginName = 'testPluginName'")
+                    {
+                        Options = { Timeout = new TimeSpan(0, 0, 60) }
+                    };
+
+                foreach (var o in searcher.Get())
+                {
+                var provider = (ManagementObject)o;
+                provider.Delete();
+                }
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                    ts.Hours, ts.Minutes, ts.Seconds,
+                                                    ts.Milliseconds);
+                Console.WriteLine($"Deleting {InsertTestCount} metrics execute result takes {elaspedTime} ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"failed to delete all wmi records: {e.ToString()}");
+            }
+             
+        }
+
+        public static void testAddWmiRecords()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            
+            var putOptions = new PutOptions
+            {
+                Timeout = new TimeSpan(0, 0, 60),
+                Type = PutType.CreateOnly
+            };
+            stopwatch.Start();
+            for (int i = 0; i < InsertTestCount; i++)
+            {
+                var metricsClass = new ManagementClass(WmiClientAgentPath, "PluginExecuteMetrics", new ObjectGetOptions());
+                ManagementObject metricsInstance = metricsClass.CreateInstance();
+                metricsInstance["PluginId"] = Guid.NewGuid().ToString();
+                metricsInstance["PluginName"] = "testPluginName";
+                metricsInstance["ExecuteTotalCount"] = 0;
+                metricsInstance["ExecuteExceptionCount"] = 0;
+                metricsInstance["ExecuteMinDuration"] = 0;
+                metricsInstance["ExecuteAvgDuration"] = 0;
+                metricsInstance["ExecuteMaxDuration"] = 0;
+                metricsInstance.Put(putOptions);
+            }
+
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                ts.Hours, ts.Minutes, ts.Seconds,
+                                                ts.Milliseconds);
+            Console.WriteLine($"Inserting {InsertTestCount} metrics execute result takes {elaspedTime} ");
+
+        }
+
+        public static void testUpdateWmiRecords()
+        {
+            try
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var searcher = new ManagementObjectSearcher(
+                   WmiClientAgentPath,
+                   $"SELECT * FROM PluginExecuteMetrics WHERE PluginName = 'testPluginName'")
+                {
+                    Options = { Timeout = new TimeSpan(0, 0, 60) }
+                };
+
+                foreach (ManagementObject provider in searcher.Get())
+                {
+                    provider.SetPropertyValue("ExecuteTotalCount", 1);
+                    provider.SetPropertyValue("ExecuteAvgDuration", 1);
+                    provider.SetPropertyValue("ExecuteMinDuration", 1);
+                    provider.SetPropertyValue("ExecuteMaxDuration", 1);
+                    provider.SetPropertyValue("ExecuteExceptionCount", 1);
+
+                    provider.Put();
+                }
+
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                    ts.Hours, ts.Minutes, ts.Seconds,
+                                                    ts.Milliseconds);
+                Console.WriteLine($"Updating {InsertTestCount} metrics execute result takes {elaspedTime} ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"failed to update the records: {ex.ToString()}");
+            }
+            
+        }
+
+        public static void testSearchAndCastRecord()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                var searcher = new ManagementObjectSearcher(
+                       WmiClientAgentPath,
+                       $"SELECT * FROM PluginExecuteMetrics WHERE PluginName = 'testPluginName'")
+                {
+                    Options = { Timeout = new TimeSpan(0, 0, 60) }
+                };
+
+                foreach (ManagementObject provider in searcher.Get())
+                {
+
+                    var metrics = new PluginExecuteMetrics
+                    {
+                        PluginId = provider["PluginId"].ToString(),
+                        PluginName = provider["PluginName"].ToString(),
+                        ExecuteTotalCount = Convert.ToInt32(provider["ExecuteTotalCount"]?.ToString()),
+                        ExecuteExceptionCount = Convert.ToInt32(provider["ExecuteExceptionCount"]?.ToString()),
+                        ExecuteMinDuration = Convert.ToInt64(provider["ExecuteMinDuration"]?.ToString()),
+                        ExecuteAvgDuration = Convert.ToInt64(provider["ExecuteAvgDuration"]?.ToString()),
+                        ExecuteMaxDuration = Convert.ToInt64(provider["ExecuteMaxDuration"]?.ToString())
+                    };
+
+                }
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                string elaspedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                                    ts.Hours, ts.Minutes, ts.Seconds,
+                                                    ts.Milliseconds);
+                Console.WriteLine($"Searching and casting {InsertTestCount} metrics execute result takes {elaspedTime} ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("failed to cast the record");
+            }
+        }
+    }
+
+    class PluginExecuteMetrics
+    {
+        public string PluginId { get; set; }
+        public string PluginName { get; set; }
+        public int ExecuteTotalCount { get; set; }
+        public int ExecuteExceptionCount { get; set; }
+        public long ExecuteMinDuration { get; set; }
+        public long ExecuteAvgDuration { get; set; }
+        public long ExecuteMaxDuration { get; set; }
+
+        public override string ToString() => this.PluginId+this.PluginName+this.ExecuteTotalCount;
+
+    }
+}
